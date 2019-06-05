f26cfd779ea31e770ba65c6b946a5137db133492
Jared Bruni <jaredbruni@gmail.com>
2019-06-04 19:11:19 -0700

    removed files

--- test05-objects/main.cpp
+++
@@ -1,6 +0,0 @@
-#include"test_output.hpp"
-
-int main() {
-    sayHello("HELLO WITH CMake");
-    return 0;
-}
--- test05-objects/test_output.cpp
+++
@@ -1,5 +0,0 @@
-#include "test_output.hpp"
-
-void sayHello(const std::string &s) {
-    std::cout << "Jared says: " << s << "\n";
-}
