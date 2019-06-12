bfdd37e795e82b0bcf7086b1f0597d47ddad6526
Jared Bruni <jaredbruni@gmail.com>
2019-06-12 10:22:53 -0700

    changed 17 to 11

---
+++ test02-library/build/CMakeFiles/catstring.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/catstring.dir/hello.cpp.o"
+  "libcatstring.pdb"
+  "libcatstring.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/catstring.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
